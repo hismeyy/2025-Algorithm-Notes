8ec7b650d689581364dd66bdbe03184d157a5955
MaxCosmos <renyh2001@outlook.com>
2025-01-09 21:41:14 +0800

    202501090941

--- 算法练习/左程云的算法通关课/demo02/Sort.java
+++ 算法练习/左程云的算法通关课/demo02/Sort.java
@@ -1,11 +1,73 @@
+import java.util.Arrays;
+
 public class Sort {
 	
 	public static void main (String[] args) {
-		int[] arr = {5, 3, 2, 4, 1};
-		insertionSort(arr);
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
+		int testCount = 10;
+		int arrLength = 10;
+		int v = 10;
+
+		System.out.println("开始测试");
+		while(testCount > 0) {
+			
+			int n = (int) (Math.random() * arrLength + 1);
+			int[] arr = getNewArray(n, v);
+			int[] arr1 = copyNewArray(arr);
+			int[] arr2 = copyNewArray(arr);
+			int[] arr3 = copyNewArray(arr);
+			selectSort(arr1);
+			bubbleSort(arr2);
+			insertionSort(arr3);
+			if (!(sameArr(arr1, arr2) && sameArr(arr1, arr3) && sameArr(arr2, arr3))) {
+				System.out.println("出错了");
+				System.out.println("arr: " + Arrays.toString(arr));
+				System.out.println("arr1: " + Arrays.toString(arr1));
+				System.out.println("arr2: " + Arrays.toString(arr2));
+				System.out.println("arr3: " + Arrays.toString(arr3));
+				break;
+			}
+
+			testCount--;
+
 		}
+		System.out.println("结束测试");
+		
+
+	}
+
+	/**
+	n 数组长度
+	v 数组范围
+	*/
+	public static int[] getNewArray(int n, int v){
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			// 1 ~ v
+			arr[i] = (int) (Math.random() * v + 1);
+		}
+		return arr;
+	}
+
+	public static int[] copyNewArray(int[] arr){
+		int[] newArr = new int[arr.length];
+		for (int i = 0; i < newArr.length; i++) {
+			newArr[i] = arr[i];
+		}
+		return newArr;
+	}
+
+	public static boolean sameArr(int[] arr1, int[] arr2){
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+
+		for(int i = 0; i < arr1.length; i++){
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	public static void swap (int[] arr, int x, int y) {
